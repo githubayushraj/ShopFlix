fe55cc1e45fc9341b94c9dc410978f274bb2ee5d
AYUSH RAJ <136356003+githubayushraj@users.noreply.github.com>
2025-03-31 01:13:56 +0530

    Delete src/main/java/com/ayush/ShopFlixBackend/ShopFlixBackend directory
    remove duplicate file

--- src/main/java/com/ayush/ShopFlixBackend/ShopFlixBackend/Repo/KitchenStorageRepo.java
+++
@@ -1,8 +0,0 @@
-package com.ayush.ShopFlixBackend.Repo;
-
-import com.ayush.ShopFlixBackend.entity.KitchenStorage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KitchenStorageRepo extends JpaRepository<KitchenStorage,Long> {
-
-}
--- src/main/java/com/ayush/ShopFlixBackend/ShopFlixBackend/Repo/WomensClothRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ayush.ShopFlixBackend.Repo;
-
-import com.ayush.ShopFlixBackend.entity.WomensCloth;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WomensClothRepo extends JpaRepository<WomensCloth,Long> {
-}
